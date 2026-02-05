2c5d12b0890562fe1e7d97043729206ae9cbaafd
Bobur-Boboyev <bbyvvv.ai@gmail.com>
2026-02-05 17:57:21 +0500

    updated

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 import requests
 from config import TOKEN
 
@@ -14,23 +13,18 @@ class EchoBot:
             "offset": self.offset,
             "limit": 10
         }
-        response = requests.get(url, params=params)
-        return response.json()["result"]
+        return requests.get(url, params=params).json()["result"]
 
-    def send(self, method: str, chat_id: int, file_id: str = None, text: str = None):
+    def send(self, method: str, chat_id: int, **kwargs):
         url = f"{self.BASE_URL}/{method}"
         data = {"chat_id": chat_id}
-        if text:
-            data["text"] = text
-        if file_id:
-            data[method.replace("send", "").lower()] = file_id
-        requests.post(url, data=data)
+        data.update(kwargs)
+        requests.get(url, data=data)
 
     def start_polling(self):
 
         while True:
             updates = self.get_updates()
-            time.sleep(1)
 
             for update in updates:
                 message = update.get("message")
@@ -42,26 +36,72 @@ class EchoBot:
                 if "text" in message:
                     self.send("sendMessage", chat_id, text=message["text"])
 
-                if "photo" in message:
-                    self.send("sendPhoto", chat_id, file_id=message["photo"][-1]["file_id"])
+                elif "photo" in message:
+                    self.send("sendPhoto", chat_id, photo=message["photo"][-1]["file_id"])
 
-                if "audio" in message:
-                    self.send("sendAudio", chat_id, file_id=message["audio"]["file_id"])
+                elif "audio" in message:
+                    self.send("sendAudio", chat_id, audio=message["audio"]["file_id"])
 
-                if "document" in message:
-                    self.send("sendDocument", chat_id, file_id=message["document"]["file_id"])
+                elif "document" in message:
+                    self.send("sendDocument", chat_id, document=message["document"]["file_id"])
 
-                if "video" in message:
-                    self.send("sendVideo", chat_id, file_id=message["video"]["file_id"])
+                elif "video" in message:
+                    self.send("sendVideo", chat_id, video=message["video"]["file_id"])
 
-                if "animation" in message:
-                    self.send("sendAnimation", chat_id, file_id=message["animation"]["file_id"])
+                elif "animation" in message:
+                    self.send("sendAnimation", chat_id, animation=message["animation"]["file_id"])
 
-                if "voice" in message:
-                    self.send("sendVoice", chat_id, file_id=message["voice"]["file_id"])
+                elif "voice" in message:
+                    self.send("sendVoice", chat_id, voice=message["voice"]["file_id"])
+
+                elif "video_note" in message:
+                    self.send("sendVideoNote", chat_id, video_note=message["video_note"]["file_id"])
+
+                elif "location" in message:
+                    loc = message["location"]
+                    self.send(
+                        "sendLocation",
+                        chat_id,
+                        latitude=loc["latitude"],
+                        longitude=loc["longitude"]
+                    )
+
+                elif "venue" in message:
+                    v = message["venue"]
+                    self.send(
+                        "sendVenue",
+                        chat_id,
+                        latitude=v["location"]["latitude"],
+                        longitude=v["location"]["longitude"],
+                        title=v["title"],
+                        address=v["address"]
+                    )
+
+                elif "contact" in message:
+                    c = message["contact"]
+                    self.send(
+                        "sendContact",
+                        chat_id,
+                        phone_number=c["phone_number"],
+                        first_name=c["first_name"]
+                    )
+
+                elif "poll" in message:
+                    p = message["poll"]
+                    options = [o["text"] for o in p["options"]]
+                    self.send(
+                        "sendPoll",
+                        chat_id,
+                        question=p["question"],
+                        options=options
+                    )
+
+                elif "dice" in message:
+                    self.send("sendDice", chat_id)
 
                 self.offset = update["update_id"] + 1
 
 
 bot = EchoBot(TOKEN)
 bot.start_polling()
+
